cacd5ae644ddf4532a57e1e65f7041f51aafe569
David-Ma40125219 <a0961100982@gmail.com>
2020-04-15 00:13:09 +0800

    First integrate the telegram and spreadsheet

--- main.py
+++ main.py
@@ -1,17 +1,215 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to send timed Telegram messages.
+
+This Bot uses the Updater class to handle the bot and the JobQueue to send
+timed messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Alarm Bot example, sends a message after a set time.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from __future__ import print_function
+
 import configparser
+
+import argparse,os
+import logging
+
 from telegram.ext import Updater, CommandHandler
 
-def hello(bot, update):
-    update.message.reply_text(
-        'hello, {}'.format(update.message.from_user.first_name))
 
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+# for overwrite callback function
+from functools import partial
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
+
+
+def alarm(context):
+    """Send the alarm message."""
+    job = context.job
+    context.bot.send_message(job.context, text='Beep!')
+
+
+def set_timer(update, context):
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+
+        # Add job to queue and stop current one if there is a timer already
+        if 'job' in context.chat_data:
+            old_job = context.chat_data['job']
+            old_job.schedule_removal()
+        new_job = context.job_queue.run_once(alarm, due, context=chat_id)
+        context.chat_data['job'] = new_job
+
+        update.message.reply_text('Timer successfully set!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def unset(update, context):
+    """Remove the job if the user changed their mind."""
+    if 'job' not in context.chat_data:
+        update.message.reply_text('You have no active timer')
+        return
+
+    job = context.chat_data['job']
+    job.schedule_removal()
+    del context.chat_data['job']
+
+    update.message.reply_text('Timer successfully unset!')
+
+def get_sheet_api(update, context):
+    chat_id = update.message.chat_id
+    try:
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+        else:
+            print('Name, Major:')
+            for row in values:
+                # Print columns A and E, which correspond to indices 0 and 4.
+                print('%s' % (row[0]))
+                update.message.reply_text('Sheet: %s' % (row[0]))
+        # update.message.reply_text("Hi")
+    except (IndexError, ValueError):
+        update.message.reply_text('NONO')
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def config_file(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{string} is not a valid file")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process command line arguments.')
+    parser.add_argument('-f','--config_f', type=config_file, required=True, help="Config file path")
+    return parser.parse_args()
+
+
+""" Get path of config file """ 
+parsed_args = parse_arguments()
+config_dir = parsed_args.config_f
+
+""" google API """
+"""Shows basic usage of the Sheets API.
+Prints values from a sample spreadsheet.
+"""
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(config_dir)
+gapi_sheet_id = config['GAPI']['SPREADSHEET_ID']
+gapi_sheet_scrects_file = config['GAPI']['SPREADSHEET_LIC']
+oauth_scrects_file = config['OAUTH']['OAUTH_LIC']
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-updater = Updater(config['TELEGRAM']['ACCESS_TOKEN'], use_context=True)
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = gapi_sheet_id
+SAMPLE_RANGE_NAME = 'Class!A:E'
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            oauth_scrects_file, SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
 
-updater.start_polling()
-updater.idle()
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+
+
+def main():
+
+
+
+
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(config['TELEGRAM']['ACCESS_TOKEN'], use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
+    dp.add_handler(CommandHandler("get_sheet", get_sheet_api))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
